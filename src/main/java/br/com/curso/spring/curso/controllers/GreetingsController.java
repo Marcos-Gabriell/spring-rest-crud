372dba15c2a94d7e6fa3240f00e19a0a43f5e3c6
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-13 16:34:39 -0300

    Começando a criar end-point

--- src/main/java/br/com/curso/spring/curso/controllers/GreetingsController.java
+++ src/main/java/br/com/curso/spring/curso/controllers/GreetingsController.java
@@ -4,11 +4,14 @@ import br.com.curso.spring.curso.model.Usuario;
 import br.com.curso.spring.curso.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * A sample greetings controller to return greeting text
@@ -38,5 +41,10 @@ public class GreetingsController {
 
         return "Olá mundo " + nome;
     }
+
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+
+    }
+
 
 }
