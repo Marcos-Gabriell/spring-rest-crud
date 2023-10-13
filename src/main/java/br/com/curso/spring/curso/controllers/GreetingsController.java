99293e75c142e877ad845349f50ece378cbe2d33
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-13 11:04:59 -0300

    Correção de problema no controller

--- src/main/java/br/com/curso/spring/curso/controllers/GreetingsController.java
+++ src/main/java/br/com/curso/spring/curso/controllers/GreetingsController.java
@@ -1,5 +1,8 @@
 package br.com.curso.spring.curso.controllers;
 
+import br.com.curso.spring.curso.model.Usuario;
+import br.com.curso.spring.curso.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -21,7 +27,16 @@ public class GreetingsController {
 
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public String retornaOlaMundo(@PathVariable String nome)  {
+    public String retornaOlaMundo(@PathVariable String nome) {
+        Usuario usuario = new Usuario();
+
+        // Set the name of the Usuario object using the setNome method
+        usuario.setNome(nome);
+
+        // Save the Usuario object to the repository
+        usuarioRepository.save(usuario);
+
         return "Olá mundo " + nome;
     }
+
 }
--- src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
+++ src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package br.com.curso.spring.curso.repository;
 
-public interface UsuarioRepository {
+import br.com.curso.spring.curso.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
