96609dcfb271311440d927f3ebf59910ce6eb898
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-15 21:04:25 -0300

    Correção da class usuario repository

--- src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
+++ src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    @Query(valeu = "select u from Usuario u where u.nome like %?1%")
-    List<Usuario> bsucarPorNome(String nome);
+    @Query(value = "select u from Usuario u where u.nome like %:nome%")
+    List<Usuario> buscarPorNome(String nome);
 }
