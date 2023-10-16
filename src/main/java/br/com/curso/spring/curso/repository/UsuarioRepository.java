4ec54a08f0ed37cfbadb24fac864b7739f83fb53
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-15 21:00:02 -0300

    Começando a criar metodo de pesquisar por nome

--- src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
+++ src/main/java/br/com/curso/spring/curso/repository/UsuarioRepository.java
@@ -2,9 +2,14 @@ package br.com.curso.spring.curso.repository;
 
 import br.com.curso.spring.curso.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    @Query(valeu = "select u from Usuario u where u.nome like")
+    List<Usuario> bsucarPorNome(String nome);
 }
